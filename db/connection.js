82ccc166e2eb5237abc7690a4c803b1c4f8eeeec
ggr1992 <ggr92@hotmail.co.uk>
2023-05-12 11:39:48 +0100

    connection

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@ const { config } = require('dotenv');
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -11,7 +12,7 @@ if(ENV === 'production') {
   config.max = 2
 }
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
