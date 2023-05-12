39d02b709fe232f0960f017c7d6d0752779f3dba
ggr1992 <ggr92@hotmail.co.uk>
2023-05-12 11:36:56 +0100

    Changes made too connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,13 +6,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
-
 if(ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL
   config.max = 2
 }
 
-module.exports = new Pool();
+if (!process.env.PGDATABASE) {
+  throw new Error('PGDATABASE not set');
+}
+
+
+module.exports = new Pool(config);
